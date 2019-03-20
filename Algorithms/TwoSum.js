1a0935c111c639317381d2f20d854edd7bab1be4
Stuart Jehn <stuartjehn@gmail.com>
2019-03-19 23:34:15 -0400

    linting

--- Algorithms/TwoSum.js
+++ Algorithms/TwoSum.js
@@ -3,7 +3,7 @@ function twoNumberSum(array, targetSum) {
 	const numsSumToTarget = [];
 	for (let i = 0; i < array.length; i++) {
 		if (prevNumbers.hasOwnProperty(targetSum - array[i])) {
-			numsSumToTarget.push(array[i], targetSum - array[i])
+			numsSumToTarget.push(array[i], targetSum - array[i]);
     }
     prevNumbers[array[i]] = true;
   }
